2cee4ae5f5d21d56b3e389f51e75707d230902f4
aktsbot <box.ashishk@gmail.com>
2020-04-19 20:11:44 +0530

    Typo in user role mig

--- migrations/20200419073600-user-role.js
+++ migrations/20200419073600-user-role.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn(
-      'users',
+      'Users',
       'role',
       Sequelize.ENUM('admin', 'regular')
     );
@@ -11,7 +11,7 @@ module.exports = {
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn(
-      'users',
+      'Users',
       'role'
     );
   }
