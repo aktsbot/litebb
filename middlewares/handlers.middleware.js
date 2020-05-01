b3932331b1d880f68e5f511b411462baf255d325
aktsbot <box.ashishk@gmail.com>
2020-05-01 16:09:30 +0530

    Error handler: show errors only in dev mode

--- middlewares/handlers.middleware.js
+++ middlewares/handlers.middleware.js
@@ -11,7 +11,7 @@ const errorHandler = (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.status = err.status || 500;
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? (err.stack || '') : {};
+  res.locals.error = req.app.get('env') === 'development' ? (err.stack || '') : '';
 
   // render the error page
   res.status(err.status || 500);
